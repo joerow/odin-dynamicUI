359b17a22cc2ee1de9c6760400f3685ae47f1109
joerow <joerow@gmail.com>
2023-04-01 13:02:11 +0800

    add build element function and add the main

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-console.log("test output");
-console.log("second test");
+function buildElement(elementType, id, classList, innerHTML) {
+  const element = document.createElement(elementType);
+  if (id === undefined) {
+    id = "";
+  }
+  if (classList === undefined) {
+    classList = "";
+  }
+  if (innerHTML === undefined) {
+    innerHTML = "";
+  }
+  element.id = id;
+  element.classList.add(...classList);
+  element.innerHTML = innerHTML;
+  return element;
+}
+document.body.append(buildElement("div", "main", ["container"], "Sample text"));
+
+const main = document.getElementById("main");
