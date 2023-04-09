2842e8b6d37e97cb303d3d1fc4ab6eb4de370cda
joerow <joerow@gmail.com>
2023-04-09 18:19:43 +0800

    refactoring

--- src/index.js
+++ src/index.js
@@ -16,43 +16,42 @@ function buildElement(elementType, id, classList, innerHTML) {
   element.innerHTML = innerHTML;
   return element;
 }
-document.body.append(buildElement("div", "main", ["container"], ""));
 
-const main = document.getElementById("main");
-
-function buildNav(NumberOfNavItems, AppendId) {
-  if (NumberOfNavItems <= 1) {
-    console.log("not enough nav items");
-    return;
-  }
-  if (AppendId === undefined) {
-    console.log("need to add an ID where the nav will append");
-    return;
-  }
-  const parent = document.getElementById(AppendId);
+function buildNav() {
   const navBar = buildElement(
-    "div",
-    "nav-bar-container",
-    ["nav-bar"],
+    "nav",
+    "",
+    ["navbar"],
     '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />'
   );
-  const navMenu = buildElement(
-    "button",
-    "menu-icon",
-    "",
-    '<span class="material-symbols-outlined">menu</span>'
-  );
-  navMenu.onclick = function () {
-    console.log("clicked");
-  };
-  /*   navMenu.onmouseover = function () {
-    console.log("mouse over");
-  };
-  navMenu.onmouseleave = function () {
-    console.log("mouse left");
-  }; */
+  const navMenu = buildElement("ul", "", ["navbar-nav"], "");
   navBar.append(navMenu);
-  parent.append(navBar);
+
+  //set the number of nav items
+  const navItemNum = 4;
+  //build the nav items
+  while () {
+    
+  }
+
+  document.body.append(navBar);
 }
 
-buildNav(2, "main");
+function buildMain() {
+  document.body.append(buildElement("main", "", "", ""));
+  const main = document.querySelector("main");
+  main.appendChild(
+    buildElement("h1", "", "", "This is a typical title on the page")
+  );
+  main.appendChild(
+    buildElement(
+      "p",
+      "",
+      "",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pulvinar dolor at lorem congue, ut malesuada lorem porttitor. Suspendisse at massa commodo, scelerisque ipsum sed, rutrum tellus. Phasellus facilisis sed nisi a pulvinar. Pellentesque vestibulum feugiat tellus, ut ultrices metus hendrerit id. Fusce egestas mi est, at ultrices orci commodo quis. Integer in pharetra nibh, tempor lobortis tellus. Praesent eros neque, aliquam eget finibus at, ultrices id sapien. Mauris vitae lacus nisi.Donec iaculis, dolor in pharetra hendrerit, nunc eros viverra nisi, sed scelerisque elit velit ut dolor. Cras fringilla odio lacus, placerat elementum felis porttitor non. Proin diam est, rutrum sit amet consectetur id, viverra ut ipsum. Etiam nec tincidunt ex. Duis cursus viverra dignissim. Quisque nunc lacus, cursus vitae velit vitae, tempus auctor lorem. Phasellus egestas metus malesuada mauris ultrices venenatis. Etiam a placerat lorem.Aliquam semper orci et justo pellentesque, a aliquet nulla dapibus. Nullam a tristique turpis, ac scelerisque metus. Sed malesuada tempus enim volutpat pulvinar. Integer nisi ligula, vulputate a dignissim in, dapibus congue purus. Curabitur mollis mauris at ligula accumsan, ut luctus enim commodo. Proin bibendum ante lacus, eu dapibus enim semper id. Sed volutpat auctor augue, sed vehicula neque gravida at.  Sed convallis dictum magna, ut placerat tortor pretium at. Nunc lacinia sit amet nisi vitae semper. Maecenas vitae enim eu augue rhoncus efficitur. Curabitur et purus tristique, dignissim erat vitae, mollis eros. Vivamus tincidunt viverra tortor vel sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec eleifend ipsum quam, ac porttitor magna tempus eu. Nam ex magna, venenatis eu lacus in, sodales semper felis. Donec ut rutrum lectus. Etiam posuere sagittis nisl non convallis. Vestibulum faucibus ligula risus, id tempor nisl ultricies ut. In volutpat, sapien at ultricies sodales, arcu turpis rhoncus lorem, ac hendrerit quam elit ac dolor. Integer et ligula a dui gravida ornare. Duis sit amet ornare risus. Proin non scelerisque justo. Mauris non tellus elit.Vestibulum tempus quis metus id congue. Sed in orci sed ligula pellentesque ultricies. Vivamus eu cursus eros. Proin egestas, justo vitae dignissim condimentum, ipsum felis bibendum dui, vel aliquet risus turpis sed dui. Maecenas sagittis nulla metus, tincidunt laoreet erat consequat non. Phasellus vel est ut massa ornare posuere ut sed erat. Mauris eu rhoncus massa. Nam sed lacus feugiat, cursus elit fermentum, placerat sem. Maecenas ac ullamcorper diam, non sollicitudin arcu. Donec sem erat, porta nec dui id, maximus vehicula urna. Quisque eu dapibus est, sit amet maximus tellus. Donec rhoncus urna mattis efficitur suscipit. Curabitur accumsan nulla vitae lorem dignissim imperdiet. "
+    )
+  );
+}
+
+buildNav();
+buildMain();
