c046462067aa58aa54e7ecf4ce0868912af6fce2
joerow <joerow@gmail.com>
2023-04-14 09:55:23 +0800

    add onclick functions to nav

--- src/index.js
+++ src/index.js
@@ -43,15 +43,6 @@ function buildNav() {
   //set the nav link text
   const navLinkText = ["Logo", "Home", "Images", "link 3", "Settings"];
 
-  //set the nav link hrefs
-  const onClick = [
-    "http://localhost:8080/",
-    'console.log("clicked")',
-    "http://localhost:8080/",
-    "http://localhost:8080/",
-    "http://localhost:8080/",
-  ];
-
   //build the nav items
   let i = 0;
   while (i < navItemNum) {
@@ -62,8 +53,6 @@ function buildNav() {
       navItem = buildElement("button", "nav-" + i, ["nav-item"], "");
     }
     let navLink = buildElement("a", "nav-link-" + i, ["nav-link"], navIcons[i]);
-    //TODO onclick not working
-    navItem.onclick = onClick[i];
     let navLinkSpan = buildElement("span", "", ["link-text"], navLinkText[i]);
     navMenu.appendChild(navItem);
     navItem.appendChild(navLink);
@@ -71,11 +60,41 @@ function buildNav() {
     i++;
   }
   document.body.append(navBar);
+
+  //set the first nav function
+  let navItems = document.getElementsByClassName("nav-item");
+  navItems[0].onclick = function () {
+    if (document.querySelector("main")) {
+      document.querySelector("main").remove();
+      console.log("removed");
+      buildMain("default");
+      console.log("rebuilt");
+    } else {
+      console.log("nothing to remove");
+      buildMain("default");
+      console.log("rebuilt");
+    }
+  };
+
+  //set the second nav function
+  navItems[1].onclick = function () {
+    if (document.querySelector("main")) {
+      document.querySelector("main").remove();
+      console.log("removed");
+      buildMain("images");
+    } else {
+      console.log("nothing to remove");
+      buildMain("images");
+      console.log("rebuilt");
+    }
+  };
 }
 
 function buildMain(param) {
   if (param === "default") {
     defaultMain();
+  } else if (param === "images") {
+    images();
   }
 
   function defaultMain() {
@@ -92,6 +111,12 @@ function buildMain(param) {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pulvinar dolor at lorem congue, ut malesuada lorem porttitor. Suspendisse at massa commodo, scelerisque ipsum sed, rutrum tellus. Phasellus facilisis sed nisi a pulvinar. Pellentesque vestibulum feugiat tellus, ut ultrices metus hendrerit id. Fusce egestas mi est, at ultrices orci commodo quis. Integer in pharetra nibh, tempor lobortis tellus. Praesent eros neque, aliquam eget finibus at, ultrices id sapien. Mauris vitae lacus nisi.Donec iaculis, dolor in pharetra hendrerit, nunc eros viverra nisi, sed scelerisque elit velit ut dolor. Cras fringilla odio lacus, placerat elementum felis porttitor non. Proin diam est, rutrum sit amet consectetur id, viverra ut ipsum. Etiam nec tincidunt ex. Duis cursus viverra dignissim. Quisque nunc lacus, cursus vitae velit vitae, tempus auctor lorem. Phasellus egestas metus malesuada mauris ultrices venenatis. Etiam a placerat lorem.Aliquam semper orci et justo pellentesque, a aliquet nulla dapibus. Nullam a tristique turpis, ac scelerisque metus. Sed malesuada tempus enim volutpat pulvinar. Integer nisi ligula, vulputate a dignissim in, dapibus congue purus. Curabitur mollis mauris at ligula accumsan, ut luctus enim commodo. Proin bibendum ante lacus, eu dapibus enim semper id. Sed volutpat auctor augue, sed vehicula neque gravida at.  Sed convallis dictum magna, ut placerat tortor pretium at. Nunc lacinia sit amet nisi vitae semper. Maecenas vitae enim eu augue rhoncus efficitur. Curabitur et purus tristique, dignissim erat vitae, mollis eros. Vivamus tincidunt viverra tortor vel sodales. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec eleifend ipsum quam, ac porttitor magna tempus eu. Nam ex magna, venenatis eu lacus in, sodales semper felis. Donec ut rutrum lectus. Etiam posuere sagittis nisl non convallis. Vestibulum faucibus ligula risus, id tempor nisl ultricies ut. In volutpat, sapien at ultricies sodales, arcu turpis rhoncus lorem, ac hendrerit quam elit ac dolor. Integer et ligula a dui gravida ornare. Duis sit amet ornare risus. Proin non scelerisque justo. Mauris non tellus elit.Vestibulum tempus quis metus id congue. Sed in orci sed ligula pellentesque ultricies. Vivamus eu cursus eros. Proin egestas, justo vitae dignissim condimentum, ipsum felis bibendum dui, vel aliquet risus turpis sed dui. Maecenas sagittis nulla metus, tincidunt laoreet erat consequat non. Phasellus vel est ut massa ornare posuere ut sed erat. Mauris eu rhoncus massa. Nam sed lacus feugiat, cursus elit fermentum, placerat sem. Maecenas ac ullamcorper diam, non sollicitudin arcu. Donec sem erat, porta nec dui id, maximus vehicula urna. Quisque eu dapibus est, sit amet maximus tellus. Donec rhoncus urna mattis efficitur suscipit. Curabitur accumsan nulla vitae lorem dignissim imperdiet. "
       )
     );
+  }
+
+  function images() {
+    document.body.append(buildElement("main", "", "", ""));
+    const main = document.querySelector("main");
+    console.log(main);
   }
 }
 
